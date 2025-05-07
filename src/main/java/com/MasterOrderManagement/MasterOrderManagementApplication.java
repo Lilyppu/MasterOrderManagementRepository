2eb4dcd07243660fa2bdd2cde4e35aa887f0ee06
lilyppu <lily@ppu.co.id>
2025-05-07 10:30:22 +0700

    first commit

---
+++ src/main/java/com/MasterOrderManagement/MasterOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.MasterOrderManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MasterOrderManagement/MasterOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MasterOrderManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
